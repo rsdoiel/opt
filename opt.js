b736a50050ac03c0a3ca16c80f2225648a548f7c
R. S. Doiel <rsdoiel@usc.edu>
2012-09-11 09:34:51 -0700

    Added license file. Fixed reference from New BSD to Simplified BSD.

--- opt.js
+++ opt.js
@@ -3,7 +3,7 @@
 // @author: R. S. Doiel, <rsdoiel@gmail.com>
 // copyright (c) 2011 all rights reserved
 //
-// Released under New the BSD License.
+// Released under the Simplified BSD License.
 // See: http://opensource.org/licenses/bsd-license.php
 //
 // revision: 0.0.9
